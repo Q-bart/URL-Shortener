2d0ff8cce21f41f8e6683970584fa5f58368cccf
Dmytro Kovalchuk <dmyutro@ukr.net>
2015-06-25 14:15:41 +0300

    Додав форму реєстрації

--- loginsys/urls.py
+++ loginsys/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
 
     url(r'^login/', 'loginsys.views.login'),
     url(r'^logout/', 'loginsys.views.logout'),
+    url(r'^register/', 'loginsys.views.register'),
 ]
--- loginsys/views.py
+++ loginsys/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response, redirect
 from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 from django.core.context_processors import csrf
 
 # Create your views  here.
@@ -23,3 +24,19 @@ def login(request):
 def logout(request):
 	auth.logout(request)
 	return redirect('/')
+
+def register(request):
+	args = {}
+	args.update(csrf(request))
+	args['form'] = UserCreationForm()
+	if request.POST:
+		newuser_form = UserCreationForm(request.POST)
+		if newuser_form.is_valid():
+			newuser_form.save()
+			newuser = auth.authenticate(username=newuser_form.cleaned_data['username'],
+				password = newuser_form.cleaned_data['password2'])
+			auth.login(request, newuser)
+			return redirect('/')
+		else:
+			args['form'] = newuser_form
+	return render_to_response('register.html', args)
